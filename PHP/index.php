71513f5d84488f22777b88431c8e52808c8c2ca2
SarahViemes <brayanjedlicka7@gmail.com>
2025-06-25 08:51:18 -0300

    criação

---
+++ PHP/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Troço ae</title>
+</head>
+<body>
+    <form action = "processar.php" method = "post">
+        <label for="numero1">Número 1</label>
+        <input name="numero1" type="number">
+        <label for="numero2">Número 2</label>
+        <input name="numero2" type="number">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
---
+++ PHP/processar.php
@@ -0,0 +1,27 @@
+<?php
+
+$mysqli = new mysqli("localhost","root","root","programacao_2");
+
+if($mysqli -> connect_errno){
+    echo "Failed to connect to MySQL: " . mysqli -> connect_errno;
+    exit();
+}
+
+Somar($_POST['numero1'],$_POST['numero 2']);
+Dividir($_POST['numero1'],$_POST['numero 2']);
+Multiplicar($_POST['numero1'],$_POST['numero 2']);
+Subtrair($_POST['numero1'],$_POST['numero 2']);
+
+
+function Somar($numero1,$numero2){
+    echo $numero1 + $numero2;
+}
+function Multiplicar($numero1,$numero2){
+    echo $numero1 * $numero2;
+}
+function Dividir($numero1,$numero2){
+    echo $numero1 / $numero2;
+
+}function Subtrair($numero1,$numero2){
+    echo $numero1 - $numero2;
+}
