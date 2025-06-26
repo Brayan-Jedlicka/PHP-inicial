fc6904df7e03e6c2a2a43c13db4e063a474d1486
SarahViemes <brayanjedlicka7@gmail.com>
2025-06-26 09:26:23 -0300

    correção de bug

--- PHP/processar.php
+++ PHP/processar.php
@@ -2,15 +2,16 @@
 
 $mysqli = new mysqli("localhost","root","root","programacao_2");
 
+
 if($mysqli -> connect_errno){
     echo "Failed to connect to MySQL: " . mysqli -> connect_errno;
     exit();
 }
 
-Somar($_POST['numero1'],$_POST['numero 2']);
-Dividir($_POST['numero1'],$_POST['numero 2']);
-Multiplicar($_POST['numero1'],$_POST['numero 2']);
-Subtrair($_POST['numero1'],$_POST['numero 2']);
+Somar($_POST['numero1'],$_POST['numero2']);
+Dividir($_POST['numero1'],$_POST['numero2']);
+Multiplicar($_POST['numero1'],$_POST['numero2']);
+Subtrair($_POST['numero1'],$_POST['numero2']);
 
 
 function Somar($numero1,$numero2){
