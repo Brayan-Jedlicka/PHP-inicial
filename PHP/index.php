a730d837c5e94b3fb4754860ec6578de51383fba
SarahViemes <brayanjedlicka7@gmail.com>
2025-07-09 08:13:31 -0300

    atualização, criação da table

--- PHP/index.php
+++ PHP/index.php
@@ -6,10 +6,26 @@
     <title>Troço ae</title>
 </head>
 <body>
+    <?php
+    $connection = require("dbfactory.php");
+    $sql = "SELECT idtodo, description FROM todo";
+
+    $result = $msqli->query($sql);
+
+    echo "<table>";
+    while ($row = $result -> fetch_assoc()){
+            echo "<tr>" 
+                    . "<td>".$row["idtodo"]."</td>"
+                    . "<td>".$row["description"]."</td>"
+                ."</tr>";
+    }
+    echo "</table>";
+    ?>
     <form action = "processar.php" method = "post">
         <label for="todo-description">Digite algo</label>
         <input name="description" id="todo-description" type="text">
         <button type="submit">Enviar</button>
     </form>
+    
 </body>
 </html>
